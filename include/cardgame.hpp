14e200aaf37d3cbfb0b59902215463d0a52f20b6
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-03 16:42:08 +0100

    added new parameters on login function

--- include/cardgame.hpp
+++ include/cardgame.hpp
@@ -8,21 +8,23 @@ CONTRACT cardgame : public contract {
     using contract::contract;
 
     cardgame(name receiver, name code, datastream<const char*> ds):
-    contract(receiver, code, ds), _users(receiver, receiver.value){}
+    contract(receiver, code, ds), _users(receiver, receiver.value) {}
 
-    ACTION login(name username);
+    ACTION login(name username, uint64_t win_count, uint64_t lost_count);
+   
     
   private:
     TABLE user_info {
       name        username;
-      uint64_t    win_count = 0;
-      uint64_t    lost_count = 0;
+      uint64_t    win_count;
+      uint64_t    lost_count;
 
       auto primary_key() const { return username.value; }
     };
     typedef multi_index<name("users"), user_info> users_table;
     
+    //Table name
     users_table _users;
-
+   
 };
 
--- src/cardgame.cpp
+++ src/cardgame.cpp
@@ -1,7 +1,7 @@
 #include <cardgame.hpp>
 #include <gameplay.cpp>
 
-ACTION cardgame::login(name username) {
+ACTION cardgame::login(name username, uint64_t win_count, uint64_t lost_count) {
   require_auth(username);
 
   // Find the record from _users table
@@ -10,9 +10,12 @@ ACTION cardgame::login(name username) {
   // Create a user record if it does not exist
     user_iterator = _users.emplace(username, [&](auto&new_user) {
         new_user.username = username;
+        new_user.win_count= win_count;
+        new_user.lost_count = lost_count;
     });
     
   }
   
 }
 
+ 
