481548f4226eda4aab4c88c0776d2dc684ae72d2
clarapic5 <clara.picazo@est.fib.upc.edu>
2019-12-03 16:57:26 +0100

    new table with int primary key

--- include/cardgame.hpp
+++ include/cardgame.hpp
@@ -8,9 +8,10 @@ CONTRACT cardgame : public contract {
     using contract::contract;
 
     cardgame(name receiver, name code, datastream<const char*> ds):
-    contract(receiver, code, ds), _users(receiver, receiver.value) {}
+    contract(receiver, code, ds), _users(receiver, receiver.value), _games(receiver, receiver.value) {}
 
     ACTION login(name username, uint64_t win_count, uint64_t lost_count);
+    ACTION game(uint64_t gameid, bool isenabled, name username);
    
     
   private:
@@ -25,6 +26,16 @@ CONTRACT cardgame : public contract {
     
     //Table name
     users_table _users;
-   
+
+    TABLE game_struct {
+      uint64_t gameid = 0;
+       bool isenabled;
+       name username;
+
+        auto primary_key() const { return gameid; }
+    };
+    typedef multi_index<name("games"), game_struct> game_table;
+
+    game_table _games;
 };
 
--- src/cardgame.cpp
+++ src/cardgame.cpp
@@ -18,4 +18,19 @@ ACTION cardgame::login(name username, uint64_t win_count, uint64_t lost_count) {
   
 }
 
- 
+ACTION cardgame::game(uint64_t gameid, bool isenabled, name username) {
+  require_auth(username);
+
+  // Find the record from _users table
+  auto game_iterator = _games.find(gameid);
+  if(game_iterator == _games.end()) {
+  // Create a user record if it does not exist
+    game_iterator = _games.emplace(username, [&](auto&new_game) {
+        new_game.gameid = gameid;
+        new_game.isenabled= isenabled;
+        new_game.username = username;
+    });
+    
+  }
+  
+}
